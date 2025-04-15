09633b1a0990b542c83ffffb2eea8851f1c5b92a
vzelinka <vzelinka2021@itcollege.lviv.ua>
2025-04-15 06:13:59 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+class Figure:
+    def __init__(self, type, length) -> None:
+        assert length > 0, "Довжина має бути більшою за 0!"
+        assert type in ["квадрат", "прямокутник", "трикутник"], "Дозволені фігури: квадрат, прямокутник, трикутник"
+        self.type = type
+        self.length = length
+
+    def get_angles(self):
+        if self.type == "трикутник":
+            return 3
+        return 4
